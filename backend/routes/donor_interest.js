e58917873b0f1ecf5a4dc38af1622689e5e21bed
unknown <avinashupadhya99@gmail.com>
2020-11-22 01:34:19 +0530

    Email notification changes and request schema changed
    Request will store doctor email

--- backend/routes/donor_interest.js
+++ backend/routes/donor_interest.js
@@ -3,20 +3,20 @@ const { v4: uuidv4 } = require('uuid');
 
 const passport = require("passport");
 const dbClient = require('../middleware/database_connection');
+const notification = require('../middleware/notification');
 const router = express.Router();
 
 router.post("/new", (req, res, next) => {
-    const { requestid, email, mobilenumber, blood_group, date_of_birth, weight, storeinfo, state, latitude, longitude } = req.body;
+    const { requestid, donorid, blood_group, doctor_email } = req.body;
 
     dbClient.execute("SELECT * FROM request WHERE requestid = ? ", [requestid], { prepare: true })
     .then(result => {
     	if(result.rowLength > 0 ) {
-    		if(storeinfo) {
-    			// Store info
-    		}
-
     		dbClient.execute("INSERT INTO donor_interest (interestid, donorid, requestid, created_date) VALUES (uuid(), ?, ?, dateof(now())", [donorid, requestid], { prepare: true })
     		.then(d_result => {
+
+                notification.sendEmail(doctor_email, `Urgent ${blood_group} blood required`, `Dear Donor,\nThere is a ${blood_group} blood requirement at ${hospital}. Please contact ${doctor_name} at ${doctor_email}.\nRegards,\nBlood Heroes`);
+
     			res.send({ message: "Donor interest notified", data: d_result })
     		})
     		.catch(d_error => {
